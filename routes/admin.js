83b0369dbf8107a4770a78bb83e273956c04dc79
1712878 <ngthotuan@gmail.com>
2019-11-04 10:48:49 +0700

    add title for admin site

--- routes/admin.js
+++ routes/admin.js
@@ -3,33 +3,33 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/pages/home/index');
+  res.render('admin/pages/home/index',{pageTitle: 'Trang chủ'});
 });
 router.get('/change-user-info', function(req, res, next) {
-  res.render('admin/pages/change-user-info/index');
+  res.render('admin/pages/change-user-info/index', {pageTitle: 'Thay đổi thông tin người dùng'});
 });
 router.get('/forgot-password', function(req, res, next) {
-  res.render('admin/pages/forgot-password/index');
+  res.render('admin/pages/forgot-password/index', {pageTitle: 'Quên mật khẩu'});
 });
 router.get('/login', function(req, res, next) {
-  res.render('admin/pages/login/index');
+  res.render('admin/pages/login/index', {pageTitle: 'Đăng nhập'});
 });
 router.get('/manager-order', function(req, res, next) {
-  res.render('admin/pages/manager-order/index');
+  res.render('admin/pages/manager-order/index', {pageTitle: 'Quản lí đơn hàng'});
 });
 router.get('/manager-product', function(req, res, next) {
-  res.render('admin/pages/manager-product/index');
+  res.render('admin/pages/manager-product/index', {pageTitle: 'Quản lí sản phẩm'});
 });
 router.get('/manager-stall', function(req, res, next) {
-  res.render('admin/pages/manager-stall/index');
+  res.render('admin/pages/manager-stall/index', {pageTitle: 'Quản lí gian hàng'});
 });
 router.get('/manager-users', function(req, res, next) {
-  res.render('admin/pages/manager-users/index');
+  res.render('admin/pages/manager-users/index', {pageTitle: 'Quản lí người dùng'});
 });
 router.get('/sales-detail', function(req, res, next) {
-  res.render('admin/pages/sales-detail/index');
+  res.render('admin/pages/sales-detail/index', {pageTitle: 'Thống kê bán hàng'});
 });
 router.get('/top10-detail', function(req, res, next) {
-  res.render('admin/pages/top10-detail/index');
+  res.render('admin/pages/top10-detail/index', {pageTitle: 'Thống kê top 10'});
 });
 module.exports = router;
