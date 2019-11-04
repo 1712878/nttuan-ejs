cc689c2c733f5877fdf5a8e773919caa02f4c795
1712878 <ngthotuan@gmail.com>
2019-11-04 08:07:25 +0700

    config web admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/login');
+  res.render('admin/pages/home/index');
 });
-
+router.get('/admin-detail', function(req, res, next) {
+  res.render('admin/pages/admin-detail/index');
+});
+router.get('/change-user-info', function(req, res, next) {
+  res.render('admin/pages/change-user-info/index');
+});
+router.get('/forgot-password', function(req, res, next) {
+  res.render('admin/pages/forgot-password/index');
+});
+router.get('/login', function(req, res, next) {
+  res.render('admin/pages/login/index');
+});
+router.get('/manager-order', function(req, res, next) {
+  res.render('admin/pages/manager-order/index');
+});
+router.get('/manager-product', function(req, res, next) {
+  res.render('admin/pages/manager-product/index');
+});
+router.get('/manager-stall', function(req, res, next) {
+  res.render('admin/pages/manager-stall/index');
+});
+router.get('/manager-users', function(req, res, next) {
+  res.render('admin/pages/manager-users/index');
+});
+router.get('/sales-detail', function(req, res, next) {
+  res.render('admin/pages/sales-detail/index');
+});
+router.get('/top10-detail', function(req, res, next) {
+  res.render('admin/pages/top10-detail/index');
+});
 module.exports = router;
